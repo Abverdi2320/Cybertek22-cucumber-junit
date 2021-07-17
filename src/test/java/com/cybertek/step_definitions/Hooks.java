d110fabac0b31f53df78d16485909621f825c841
Abbas Verdiyev <Abverdi2320@gmail.com>
2021-07-17 14:36:23 -0400

    Screen shot

--- src/test/java/com/cybertek/step_definitions/Hooks.java
+++ src/test/java/com/cybertek/step_definitions/Hooks.java
@@ -1,10 +1,9 @@
 package com.cybertek.step_definitions;
 
 import com.cybertek.utilities.Driver;
-import io.cucumber.java.After;
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
+import io.cucumber.java.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
     //import from io.cucumber.java not from Junit
@@ -14,10 +13,20 @@ public class Hooks {
     }
 
     @After
-    public void tearDownScenario(){
+    public void tearDownScenario(Scenario scenario){
+        scenario.isFailed();
+
+//        System.out.println();
+        if(scenario.isFailed()){
+            byte[] screenshot =((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png",scenario.getName());
+
+        }
+
+
+
         System.out.println("--- TearDown steps are being applied...---");
-        Driver.closeDriver();
-        System.out.println();
+      //  Driver.closeDriver();
     }
 
     @BeforeStep
